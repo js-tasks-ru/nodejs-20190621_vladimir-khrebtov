e6d105048e3ada6e19826f8a580f3858c56f4960
VladimirKh <vladimir.h@belvg.com>
2019-07-10 15:53:21 +0300

    solution 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const app = new Koa();
+const broadcast = []
 
 app.use(require('koa-static')('public'));
 app.use(require('koa-bodyparser')());
@@ -8,9 +9,29 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise((resolve) => {
+    broadcast.push(resolve);
+
+    ctx.res.on('close', () => {
+      if (ctx.res.finished) return;
+
+
+    });
+  });
+
+  ctx.body = message;
+
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  if (!message) ctx.throw(400, 'message is required');
+
+  broadcast.forEach((resolve) => resolve(message));
+
+  broadcast.length = 0;
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
