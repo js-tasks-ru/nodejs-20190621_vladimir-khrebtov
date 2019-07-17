03c99828bc39ff406d9e32e2f7fb087fe3d3dd6f
VladimirKh <vladimir.h@belvg.com>
2019-07-17 11:21:15 +0300

    solution 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,24 @@
+const Categories = require('../models/Category');
+
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = { categories: [] };
+  let result = [];
+  await Categories.find({})
+      .exec()
+      .then((categories) => {
+        // console.log(categories);
+        if (!categories) return [];
+        result = categories.map((category) => {
+          return {
+            id: category._id,
+            title: category.title,
+            subcategories: [{
+              id: category.subcategories[0]._id,
+              title: category.subcategories[0].title,
+            }],
+          };
+        });
+      })
+      .catch((error) => ctx.throw(404, 'wrong subcategory'));
+  ctx.body = { categories: result };
 };
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,56 @@
+const Products = require('../models/Product');
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = { products: [] };
+  const subCategoryName = ctx.request.query.subcategory;
+  // console.log(subCategoryName);
+  let result = [];
+  await Products.find({
+    subcategory: subCategoryName,
+  }).then((productsList) => {
+    if (!productsList) console.log(false);
+    result = productsList.map((product) => {
+      return {
+        id: product._id,
+        title: product.title,
+        images: product.images,
+        category: product.category,
+        subcategory: product.subcategory,
+        price: product.price,
+        description: product.description,
+      };
+    });
+  }).catch((error) => {
+    result = [];
+  });
+
+  ctx.body = { products: result };
 };
 
 module.exports.productList = async function productList(ctx, next) {
+  console.log(ctx.request.query);
   ctx.body = { products: [] };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = { product: {} };
+  const productId = ctx.params.id;
+  const result = {};
+
+  if (!mongoose.Types.ObjectId.isValid(productId)) ctx.throw(400, 'incorrect id');
+
+  await Products.findOne({
+    _id: productId,
+  }).then((product) => {
+    result.id = product._id;
+    result.title = product.title;
+    result.images = product.images;
+    result.category = product.category;
+    result.subcategory = product.subcategory;
+    result.price = product.price;
+    result.desciption = product.description;
+  }).catch((error) => {
+    ctx.throw(404, `Error code ${error.code}`);
+  });
+  ctx.body = { product: result };
 };
 
