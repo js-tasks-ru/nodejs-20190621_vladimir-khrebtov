592306a9ba2658e1dbc637d22549eed7c02d918d
VladimirKh <vladimir.h@belvg.com>
2019-07-18 18:17:33 +0300

    solution 7-module 2-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,22 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+const mongoose = require('mongoose');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) return done(null, false, 'Не указан email');
+
+  User.findOne({email}).then(async (user) => {
+    if (!user) {
+      const newUser = await new User({
+        email: email,
+        displayName: displayName,
+      });
+
+      await newUser.save();
+      return done(null, newUser, 'new user created');
+    }
+
+    done(null, user, 'user found');
+  }).catch((error) => {
+    return done(error, false, 'Некорректный email.');
+  });
 };
